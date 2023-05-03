39fbf8dab264bcb282838455fdb81147fb48fa6a
gourderased <gourderased@gmail.com>
2023-05-03 20:45:17 +0900

    Initial commit

---
+++ src/main/java/com/example/heronjjang/HeronJJangApplication.java
@@ -0,0 +1,13 @@
+package com.example.heronjjang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeronJJangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeronJJangApplication.class, args);
+    }
+        
+}
